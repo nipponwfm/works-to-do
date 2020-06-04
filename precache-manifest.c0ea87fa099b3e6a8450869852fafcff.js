f56aa3f597f5361751b1da18a2dbfc0529f4d764
Thanh Dong <nipponwfm@gmail.com>
2020-06-04 15:07:50 +0700

    Updates

---
+++ precache-manifest.c0ea87fa099b3e6a8450869852fafcff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de444b8bae0d4b51a33c949241e36330",
+    "url": "/works-to-do/index.html"
+  },
+  {
+    "revision": "a5399c259a0c3a83f05c",
+    "url": "/works-to-do/static/css/main.0c4d6553.chunk.css"
+  },
+  {
+    "revision": "d53b71d041248d7c8cc1",
+    "url": "/works-to-do/static/js/2.8f74c431.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/works-to-do/static/js/2.8f74c431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5399c259a0c3a83f05c",
+    "url": "/works-to-do/static/js/main.7acce145.chunk.js"
+  },
+  {
+    "revision": "c7b1649f70fe6cdabac5",
+    "url": "/works-to-do/static/js/runtime-main.d4376246.js"
+  }
+]);
